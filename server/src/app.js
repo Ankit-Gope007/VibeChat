3fcf390c452e1f31215969b45b7f580a8e57335a
Ankit Gope <ankitgope21@gmail.com>
2025-04-13 14:41:48 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -26,6 +26,13 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", req.headers.origin); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json({limit : "1000kb"}))
 app.use(express.urlencoded({extended:true,limit : "1000kb"}))
 app.use(express.static("public"))
