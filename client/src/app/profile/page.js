982d6f10b1177ff68b8df64142c8b0fdef292a1b
Ankit Gope <ankitgope21@gmail.com>
2025-04-13 16:27:55 +0530

    Update page.js

--- client/src/app/profile/page.js
+++ client/src/app/profile/page.js
@@ -101,59 +101,6 @@ const Page = () => {
     }
 
     return (
-        // <>
-
-        //     {loading ? <Loading /> :
-        //         <div className='overflow-auto min-h-screen min-w-screen flex items-center justify-center'>
-        //             <div className='h-[800px] lg:h-[500px] w-[80%] lg:2/4  border rounded-2xl shadow-2xl ' >
-        //                 <div>
-        //                     <h1 className='p-10 text-4xl font-bold text-center'>Lets Set up Your Profile</h1>
-        //                 </div>
-        //                 <div className='flex w-full gap-5 h-[300px] flex-col  lg:flex-row '
-        //                 >
-        //                     <div className=' w-full lg:w-1/2  h-[250px] gap-5   flex flex-col items-center flex-wrap mb-[70px] '>
-        //                         <div className={`pfp w-[80px]  h-[80px] mt-5 rounded-full bg-gray-200 flex justify-center items-center' ${pfp1 && 'k'}`}
-        //                             onClick={handlePfp1}
-        //                         >
-        //                             <img src="pfp1.svg" alt="" />
-        //                         </div>
-        //                         <div className={`pfp w-[80px]  h-[80px] mt-5 rounded-full bg-gray-200 flex justify-center items-center' ${pfp2 && 'k'}`}
-        //                             onClick={handlePfp2}>
-        //                             <img src="pfp2.svg" alt="" />
-        //                         </div>
-        //                         <div className={`pfp w-[80px]  h-[80px] mt-5 rounded-full bg-gray-200 flex justify-center items-center' ${pfp3 && 'k'}`}
-        //                             onClick={handlePfp3}>
-        //                             <img src="pfp3.svg" alt="" />
-        //                         </div>
-        //                         <div className={`pfp w-[80px]  h-[80px] mt-5 rounded-full bg-gray-200 flex justify-center items-center' ${pfp4 && 'k'}`}
-        //                             onClick={handlePfp4}>
-        //                             <img src="pfp4.svg" alt="" />
-        //                         </div>
-
-        //                         <h1 className='absolute my-[250px] text-2xl text-center '>Choose Your Avatar</h1>
-        //                     </div>
-        //                     <div className='  mx-5 lg:w-1/2'>
-        //                         <h1 className='ml-4 mt-7'>First Name</h1>
-        //                         <Input
-        //                             className={'rounded-full border-none shadow-lg my-3  bg-gray-50'}
-        //                             onChange={(e) => setFirstName(e.target.value)}
-        //                         // placeholder={'First Name'}
-        //                         />
-        //                         <h1 className='ml-4 mt-7'>Last Name</h1>
-        //                         <Input
-        //                             className={'rounded-full border-none shadow-lg my-3 mt-5 bg-gray-50'}
-        //                             onChange={(e) => setLastName(e.target.value)}
-        //                         // placeholder={'Last Name'}
-        //                         />
-        //                         <Button className="bg-blue-600 w-full h-[50px] hover:bg-blue-700 active:bg-blue-900 shadow-lg my-3"
-        //                             onClick={handleSaveInfo}
-        //                         >Save Info</Button>
-        //                     </div>
-
-        //                 </div>
-        //             </div>
-        //         </div>}
-        // </>
         <>
         {loading ? <Loading /> :
             <div className='overflow-auto min-h-screen w-full flex items-center justify-center p-4'>
